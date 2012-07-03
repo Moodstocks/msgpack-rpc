d1adeb73cd81d9d9bb4e58c4e9e8ace821955ba2
Pierre Chapuis <catwell@archlinux.us>
2012-07-03 18:53:54 +0200

    update.sh -> pushd/popd

--- update.sh
+++ update.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 git pull origin
-cd ruby
-rm -f pkg/msgpack-rpc*.gem
-rake build
-sudo gem install pkg/msgpack-rpc*.gem
+pushd ruby
+  rm -f pkg/msgpack-rpc*.gem
+  rake build
+  rvmsudo gem install pkg/msgpack-rpc*.gem
+popd
